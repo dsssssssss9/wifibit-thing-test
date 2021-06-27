dcd6daa76c268056551ceb8ae0413fbc5939aa54
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2021-06-27 18:38:08 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,20 @@
+input.onButtonPressed(Button.A, function () {
+    WiFiBit.executeHttpMethod(
+    HttpMethod.GET,
+    "api.thingspeak.com",
+    80,
+    "/update?api_key=M69ZVWEMWTMK9HHK&filed1=" + Dice
+    )
+    basic.showIcon(IconNames.Yes)
+})
+input.onButtonPressed(Button.B, function () {
+    Dice = randint(1, 6)
+    basic.showNumber(Dice)
+})
+let Dice = 0
+WiFiBit.connectToWiFiBit()
+WiFiBit.connectToWiFiNetwork("XHsc40000", "Ncc1701a")
+basic.showIcon(IconNames.Heart)
+basic.forever(function () {
+	
+})
